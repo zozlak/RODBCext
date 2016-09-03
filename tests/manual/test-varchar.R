c56e74b586817bff6bad376758285631358755dc
Will Beasley <wibeasley@hotmail.com>
2016-09-03 12:15:34 -0500

    tests ODBC 11.0
    To supplement the 13.1 tests
    
    Addresses #6

--- tests/manual/test-varchar.R
+++ tests/manual/test-varchar.R
@@ -1,3 +1,8 @@
+# Run this knintr::stitch_rmd line below (in the console) for easier communication to other developers.
+#   It runs everything below and save the results to an md & html file.
+#   Notice the 'sessionInfo()' at the bottom of the output.
+# knitr::stitch_rmd(script="./tests/manual/test-varchar.R", output="./tests/manual/stitched-output/test-varchar.md")
+
 rm(list=ls(all=TRUE)) #Clear the memory for any variables set from any previous runs.
 
 # ---- load-packages -----------------------------------------------------------
@@ -9,7 +14,7 @@ requireNamespace("testit")
 set.seed(234) #To make the results easier to compare.
 
 dsn_13_1 <- "rodbcext-test-13-1" #ODBC Driver 13.1: https://www.microsoft.com/en-us/download/details.aspx?id=53339
-# dsn_11_0 <- "rodbcext-test-11-0" #ODBC Driver 11: https://www.microsoft.com/en-us/download/details.aspx?id=36434
+dsn_11_0 <- "rodbcext-test-11-0" #ODBC Driver 11: https://www.microsoft.com/en-us/download/details.aspx?id=36434
 row_count <- 5L
 
 sql_delete   <- "DELETE FROM dbo.tbl_chracter_test_1"
@@ -62,3 +67,41 @@ testit::assert("The 256-character column should be correct.", identical(ds_read_
 #This fails.  All elements are 7999
 testit::assert("The 8000-character column should have.the correct length", all(nchar(ds_read_13_1$char_8000)==8000L))
 testit::assert("The 8000-character column should be correct.", identical(ds_read_13_1$char_8000, ds_write$char_8000))
+
+
+# ---- test-11-0 ---------------------------------------------------------------
+channel <- RODBC::odbcConnect(dsn_11_0)
+base::tryCatch(
+  expr = {
+    
+    RODBCext::sqlExecute(channel, sql_delete)
+    RODBCext::sqlExecute(channel, sql_insert, ds_write)
+    
+    ds_read_11_0 <- RODBCext::sqlExecute(channel, sql_retrieve, data=NULL, fetch=TRUE, stringsAsFactors=FALSE)
+    
+  }, finally = {
+    RODBC::odbcClose(channel)
+    rm(channel)
+  }
+)
+
+sapply(ds_read_11_0, function(x ) max(nchar(x)))
+# > sapply(ds_read_11_0, function(x ) max(nchar(x)))
+#        id    char_5  char_255  char_256 char_8000 
+#         1         5       255       255      7999 
+        
+testit::assert("The `id` column should be correct.", identical(ds_read_11_0$id, 1L:5L))
+
+testit::assert("The 5-character column should have.the correct length", all(nchar(ds_read_11_0$char_5)==5L))
+testit::assert("The 5-character column should be correct.", identical(ds_read_11_0$char_5, ds_write$char_5))
+
+testit::assert("The 255-character column should have.the correct length", all(nchar(ds_read_11_0$char_255)==255L))
+testit::assert("The 255-character column should be correct.", identical(ds_read_11_0$char_255, ds_write$char_255))
+
+#This fails.  All elements are 255 (not 256)
+testit::assert("The 256-character column should have.the correct length", all(nchar(ds_read_11_0$char_256)==256L))
+testit::assert("The 256-character column should be correct.", identical(ds_read_11_0$char_256, ds_write$char_256))
+
+#This fails.  All elements are 7999
+testit::assert("The 8000-character column should have.the correct length", all(nchar(ds_read_11_0$char_8000)==8000L))
+testit::assert("The 8000-character column should be correct.", identical(ds_read_11_0$char_8000, ds_write$char_8000))
