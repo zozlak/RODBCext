6ecb66848ed908e7239da9438a1ce1e09dd35163
Will Beasley <wibeasley@hotmail.com>
2016-09-03 11:59:28 -0500

    256-length in database

--- tests/manual/create-table-varchar-1.sql
+++ tests/manual/create-table-varchar-1.sql
@@ -17,6 +17,7 @@ CREATE TABLE [dbo].[tbl_chracter_test_1](
 	[id] [int] NOT NULL,
 	[char_5] [varchar](5) NOT NULL,
 	[char_255] [varchar](255) NOT NULL,
+	[char_256] [varchar](256) NOT NULL,
 	[char_8000] [varchar](8000) NOT NULL,
  CONSTRAINT [PK_tbl_chracter_test_1] PRIMARY KEY CLUSTERED 
 (
