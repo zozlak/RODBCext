77f8b413e99e35dbf78c32be498c37d26ad2c0ed
Will Beasley <wibeasley@hotmail.com>
2016-09-03 11:58:30 -0500

    isolate fail at 256-length column
    @zozlak & @nutterb, as you predicted, this works at 255, but fails at
    256-length varchars.
    
    Addresses #6

--- tests/manual/test-varchar.R
+++ tests/manual/test-varchar.R
@@ -13,13 +13,14 @@ dsn_13_1 <- "rodbcext-test-13-1" #ODBC Driver 13.1: https://www.microsoft.com/en
 row_count <- 5L
 
 sql_delete   <- "DELETE FROM dbo.tbl_chracter_test_1"
-sql_insert   <- "INSERT INTO dbo.tbl_chracter_test_1 VALUEs (?, ?, ?, ?)"
+sql_insert   <- "INSERT INTO dbo.tbl_chracter_test_1 VALUEs (?, ?, ?, ?, ?)"
 sql_retrieve <- "SELECT * FROM dbo.tbl_chracter_test_1"
 
 ds_write <- data.frame(
 	ID               = seq_len(row_count),
 	char_5           = replicate(row_count, paste(sample(letters, size=   5L, replace=T), collapse="")),
 	char_255         = replicate(row_count, paste(sample(letters, size= 255L, replace=T), collapse="")),
+	char_256         = replicate(row_count, paste(sample(letters, size= 256L, replace=T), collapse="")),
 	char_8000        = replicate(row_count, paste(sample(letters, size=8000L, replace=T), collapse="")),
 	# char_max       = replicate(row_count, paste(sample(letters, size=8001L, replace=T), collapse="")), #Is there a column/condition that makes sense for max?
 	stringsAsFactors = FALSE
@@ -43,8 +44,8 @@ base::tryCatch(
 
 sapply(ds_read_13_1, function(x ) max(nchar(x)))
 # > sapply(ds_read_13_1, function(x ) max(nchar(x)))
-#        id    char_5  char_255 char_8000 
-#         1         5       255      7999 
+#        id    char_5  char_255  char_256 char_8000 
+#         1         5       255       255      7999 
         
 testit::assert("The `id` column should be correct.", identical(ds_read_13_1$id, 1L:5L))
 
@@ -53,6 +54,10 @@ testit::assert("The 5-character column should be correct.", identical(ds_read_13
 
 testit::assert("The 255-character column should have.the correct length", all(nchar(ds_read_13_1$char_255)==255L))
 testit::assert("The 255-character column should be correct.", identical(ds_read_13_1$char_255, ds_write$char_255))
+
+#This fails.  All elements are 255 (not 256)
+testit::assert("The 256-character column should have.the correct length", all(nchar(ds_read_13_1$char_256)==256L))
+testit::assert("The 256-character column should be correct.", identical(ds_read_13_1$char_256, ds_write$char_256))
 
 #This fails.  All elements are 7999
 testit::assert("The 8000-character column should have.the correct length", all(nchar(ds_read_13_1$char_8000)==8000L))
